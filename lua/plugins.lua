ebb4d117e6531f9a9be5c6bb84a919eeca481e16
AndyLu <laserlue@gmail.com>
2022-08-07 01:51:31 +0800

    add plugin(nvim-markdown)

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -126,4 +126,12 @@ return require('packer').startup(function()
         }
       end
     }
+
+    -- nvim-markdown
+    use({
+      "iamcco/markdown-preview.nvim", 
+      run = "cd app && npm install", 
+      setup = function() vim.g.mkdp_filetypes = { "markdown" } end, 
+      ft = { "markdown" }, 
+    })
 end)
