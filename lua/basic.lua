03c6e63db38976b66f5a10237b24f2c4df9808a9
AndyLu <laserlue@gmail.com>
2022-09-08 17:28:38 +0800

    modify comments in basic.lua

--- lua/basic.lua
+++ lua/basic.lua
@@ -5,7 +5,8 @@ vim.g.encoding = "UTF-8"
 vim.o.fileencoding = "utf-8"
 -- jkhl 移动时光标周围保留8行
 vim.o.scrolloff = 8
-vim.o.sidescrolloff = 8 -- 使用相对行号
+vim.o.sidescrolloff = 8
+-- 使用相对行号
 vim.wo.number = true
 vim.wo.relativenumber = true
 -- 高亮所在行
