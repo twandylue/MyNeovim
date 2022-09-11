76b17912e571da0e7545d56b2853df1ed13756a0
AndyLu <laserlue@gmail.com>
2022-09-12 07:41:09 +0800

    update nvim-lspsaga settings

--- lua/plugin-config/nvim-lspsaga.lua
+++ lua/plugin-config/nvim-lspsaga.lua
@@ -7,4 +7,12 @@ end
 saga.init_lsp_saga {
   server_filetype_map = {},
   code_action_lightbulb = { enable = false, },
+  border_style = "rounded",
+  finder_action_keys = {
+    open = "o",
+    vsplit = "<C-v>",
+    split = "<C-i>",
+    tabe = "t",
+    quit = "q",
+  },
 }
