b84bbf8ec223828dacdbd3dcb8db81ca8a94a1e2
AndyLu <laserlue@gmail.com>
2022-09-18 21:57:36 +0800

    modify keybindings of terminal mode

--- lua/plugin-config/nvim-toggleterm.lua
+++ lua/plugin-config/nvim-toggleterm.lua
@@ -30,7 +30,7 @@ toggleterm.setup {
 function _G.set_terminal_keymaps()
   local opts = { buffer = 0 }
   vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', '<tab><tab>', [[<Esc>]], opts)
+  vim.keymap.set('t', '<C-[>', [[<Esc>]], opts)
   vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
   vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
   vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
