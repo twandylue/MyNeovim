f9ccdbf77683fb1ba78b053ef31478eb87dfe0d5
AndyLu <laserlue@gmail.com>
2022-10-03 08:47:01 +0800

    add comment for neorg

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -207,7 +207,9 @@ packer.startup(function(use)
   -- neorg
   use {
     "nvim-neorg/neorg",
-    -- run = ":Neorg sync-parsers", -- This is the important bit!
+    -- important: export the CC(support for c++14) variable in general: export CC=/path/to/newer/compiler
+    -- ref: https://github.com/nvim-neorg/neorg
+    run = ":Neorg sync-parsers", -- This is the important bit!
     ft = "norg",
     requires = "nvim-lua/plenary.nvim"
   }
