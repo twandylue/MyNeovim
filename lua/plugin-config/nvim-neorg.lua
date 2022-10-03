fe83b25ee7af8f9eae0cfa79162109e41865ab67
AndyLu <laserlue@gmail.com>
2022-10-03 22:29:21 +0800

    restore some settings to be compatible nvim 0.7

--- lua/plugin-config/nvim-neorg.lua
+++ lua/plugin-config/nvim-neorg.lua
@@ -1,18 +1,18 @@
-local status, neorg = pcall(require, 'neorg')
-if (not status) then
-  vim.notify("neorg is not installed", "error")
-  print("neorg is not installed")
-  return
-end
-
-neorg.setup {
-  load = {
-    ["core.defaults"] = {},
-    ["core.norg.concealer"] = {},
-    -- ["core.presenter"] = {
-    --   config = { -- Note that this table is optional and doesn't need to be provided
-    --       -- Configuration here
-    --   }
-    -- }
-  }
-}
+-- local status, neorg = pcall(require, 'neorg')
+-- if (not status) then
+--   vim.notify("neorg is not installed", "error")
+--   print("neorg is not installed")
+--   return
+-- end
+--
+-- neorg.setup {
+--   load = {
+--     ["core.defaults"] = {},
+--     ["core.norg.concealer"] = {},
+--     -- ["core.presenter"] = {
+--     --   config = { -- Note that this table is optional and doesn't need to be provided
+--     --       -- Configuration here
+--     --   }
+--     -- }
+--   }
+-- }
