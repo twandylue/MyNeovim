6972d4163822f4594eb5c549bb7052187dee4e65
AndyLu <laserlue@gmail.com>
2022-10-06 14:47:57 +0800

    update foramt setting in nvim-null-ls

--- lua/plugin-config/nvim-null-ls.lua
+++ lua/plugin-config/nvim-null-ls.lua
@@ -1,26 +1,20 @@
 local status, null_ls = pcall(require, "null-ls")
 if not status then
-	vim.notify("null-ls is not installed", vim.log.levels.ERROR)
-	print("null-ls is not installed")
-	return
+  vim.notify("null-ls is not installed", vim.log.levels.ERROR)
+  print("null-ls is not installed")
+  return
 end
 
+local sources = {
+  null_ls.builtins.formatting.stylua.with({
+    extra_args = { "--indent-type", "Spaces" }
+  }),
+  null_ls.builtins.formatting.csharpier,
+}
+
 if vim.fn.has("mac") == 1 then
-	null_ls.setup({
-		sources = {
-			-- install rustfrm: https://github.com/rust-lang/rustfmt
-			-- through rustup: https://github.com/rust-lang/rustup
-			null_ls.builtins.formatting.rustfmt,
-			null_ls.builtins.formatting.stylua,
-			null_ls.builtins.formatting.csharpier,
-			null_ls.builtins.completion.luasnip,
-		},
-	})
-else
-	null_ls.setup({
-		sources = {
-			null_ls.builtins.formatting.stylua,
-			null_ls.builtins.formatting.csharpier,
-		},
-	})
+  table.insert(sources, null_ls.builtins.formatting.rustfmt)
+  table.insert(sources, null_ls.builtins.formatting.luasnip)
 end
+
+null_ls.setup(sources)
