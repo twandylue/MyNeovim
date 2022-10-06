0d790c6d1cf562d6aafce494852e0b9e8c6b88d7
AndyLu <laserlue@gmail.com>
2022-10-06 22:59:53 +0800

    formating lsp-signature.lua

--- lua/plugin-config/lsp/lsp-signature.lua
+++ lua/plugin-config/lsp/lsp-signature.lua
@@ -1,14 +1,14 @@
 local status_ok, lsp_signature = pcall(require, "lsp_signature")
 if not status_ok then
-	vim.notify("lsp_signature is not installed", vim.log.levels.ERROR)
-	print("lsp_signature is not installed")
-	return
+  vim.notify("lsp_signature is not installed", vim.log.levels.ERROR)
+  print("lsp_signature is not installed")
+  return
 end
 
 lsp_signature.setup({
-	bind = true,
-	handler_opts = { border = "rounded" },
-	hint_prefix = " ",
-	-- hi_parameter = "IncSearch",
-	hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
+  bind = true,
+  handler_opts = { border = "rounded" },
+  hint_prefix = " ",
+  -- hi_parameter = "IncSearch",
+  hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
 })
