a89fe8274e01a967e2b6ac651c28eae52823c1b9
AndyLu <laserlue@gmail.com>
2022-10-07 01:18:19 +0800

    re-format nvim-indent.lua

--- lua/plugin-config/nvim-indent.lua
+++ lua/plugin-config/nvim-indent.lua
@@ -13,8 +13,8 @@ end
 -- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
 
 vim.opt.list = true
-vim.opt.listchars:append "space:⋅"
-vim.opt.listchars:append "eol:↴"
+vim.opt.listchars:append("space:⋅")
+vim.opt.listchars:append("eol:↴")
 
 blankline.setup({
   show_current_context = true,
