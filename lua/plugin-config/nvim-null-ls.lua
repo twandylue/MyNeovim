3a4c67ebb30451a077edf37e3e86ba4db7b85e52
AndyLu <laserlue@gmail.com>
2022-10-06 15:26:58 +0800

    update setting of nvim-null-ls(space=2)

--- lua/plugin-config/nvim-null-ls.lua
+++ lua/plugin-config/nvim-null-ls.lua
@@ -1,23 +1,23 @@
 local status, null_ls = pcall(require, "null-ls")
 if not status then
-    vim.notify("null-ls is not installed", vim.log.levels.ERROR)
-    print("null-ls is not installed")
-    return
+  vim.notify("null-ls is not installed", vim.log.levels.ERROR)
+  print("null-ls is not installed")
+  return
 end
 
 local sources = {
-    null_ls.builtins.formatting.stylua.with({
-        extra_args = { "--indent-type", "Spaces" },
-    }),
-    null_ls.builtins.formatting.csharpier,
+  null_ls.builtins.formatting.stylua.with({
+    extra_args = { "--indent-type", "Spaces", "--indent-width", "2" },
+  }),
+  null_ls.builtins.formatting.csharpier,
 }
 
 if vim.fn.has("mac") == 1 then
-    table.insert(sources, null_ls.builtins.formatting.rustfmt)
-    -- TODO: check luasnip
-    -- table.insert(sources, null_ls.builtins.formatting.luasnip)
+  table.insert(sources, null_ls.builtins.formatting.rustfmt)
+  -- TODO: check luasnip
+  -- table.insert(sources, null_ls.builtins.formatting.luasnip)
 end
 
 null_ls.setup({
-    sources = sources,
+  sources = sources,
 })
