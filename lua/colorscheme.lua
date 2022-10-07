fe2899700cbc87d5b15b2e08784a78701fe58334
AndyLu <laserlue@gmail.com>
2022-10-07 11:23:18 +0800

    fix in colorscheme.lua

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,5 +1,5 @@
 local status_gruvbox, gruvbox = pcall(require, "gruvbox")
-if not status then
+if not status_gruvbox then
   vim.notify("Color chemem: gruvbox is not installed")
   print("Color chemem: Gruvbox is not installed")
 end
