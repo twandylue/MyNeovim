5edffa57036d43b6f5ed51258a06855aecafbc01
AndyLu <laserlue@gmail.com>
2022-10-09 09:04:33 +0800

    update ensure_installed list in mason lspconfig

--- lua/plugin-config/nvim-mason.lua
+++ lua/plugin-config/nvim-mason.lua
@@ -1,30 +1,43 @@
 local status, mason = pcall(require, "mason")
 if not status then
-	print("mason is not installed")
-	return
+  print("mason is not installed")
+  return
 end
 
 local status2, masonLspconfig = pcall(require, "mason-lspconfig")
 if not status2 then
-	print("mason-lspconfig is not installed")
-	return
+  print("mason-lspconfig is not installed")
+  return
 end
 
 mason.setup({})
 
 masonLspconfig.setup({
-	ensure_installed = {
-		"jsonls",
-		"stylua",
-		"lua-language-server",
-		"tsserver",
-		"pyright",
-		"yamlls",
-		"bashls",
-		"csharp-language-server",
-		"csharpier",
-		-- "omnisharp",
-		"rust_analyzer",
-		"clangd",
-	},
+  automatic_installation = true,
+  ensure_installed = {
+    "shellcheck",
+    "bash-language-server",
+    "clangd",
+    "cmake-language-server",
+    "csharpier",
+    "css-lsp",
+    "cssmodules-language-server",
+    "dockerfile-language-server",
+    "fixjson",
+    "html-lsp",
+    "json-lsp",
+    "lua-language-server",
+    "markdownlint",
+    "marksman",
+    "omnisharp",
+    "omnisharp-mono",
+    "pyright",
+    "rust-analyzer",
+    "sqls",
+    "stylua",
+    "typescript-language-server",
+    "vim-language-server",
+    "yaml-language-server",
+    "yamllint",
+  },
 })
