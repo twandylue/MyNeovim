d641ee56d47b79b212712133846f84be2492f1ea
AndyLu <laserlue@gmail.com>
2022-10-10 21:18:34 +0800

    update settings in gruvbox

--- lua/colorschemes/gruvbox.lua
+++ lua/colorschemes/gruvbox.lua
@@ -1,6 +1,10 @@
 local status_gruvbox, gruvbox = pcall(require, "gruvbox")
 if not status_gruvbox then
   vim.notify("Color chemem: gruvbox is not installed")
-  print("Color chemem: Gruvbox is not installed")
+  print("Color chemem: gruvbox is not installed")
   return
 end
+
+gruvbox.setup({
+  contrast = "true", -- can be "hard", "soft" or empty string
+})
