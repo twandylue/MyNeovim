f944aa8e56d64f1aad8da3ba0d219ef14ddcc202
AndyLu <laserlue@gmail.com>
2022-10-31 18:49:57 +0800

    reformat nvim-lspsaga.lua

--- lua/plugin-config/nvim-lspsaga.lua
+++ lua/plugin-config/nvim-lspsaga.lua
@@ -1,22 +1,22 @@
 local status, saga = pcall(require, "lspsaga")
 if not status then
-	print("lspsaga is not installed")
-	return
+  print("lspsaga is not installed")
+  return
 end
 
 saga.init_lsp_saga({
-	server_filetype_map = {},
-	code_action_lightbulb = { enable = false },
-	border_style = "rounded",
-	finder_action_keys = {
-		open = "<Enter>",
-		vsplit = "<C-v>",
-		split = "<C-s>",
-		tabe = "t",
-		quit = "q",
-	},
-	symbol_in_winbar = {
-		enable = true,
-		in_custom = false,
-	},
+  server_filetype_map = {},
+  code_action_lightbulb = { enable = false },
+  border_style = "rounded",
+  finder_action_keys = {
+    open = "<Enter>",
+    vsplit = "<C-v>",
+    split = "<C-s>",
+    tabe = "t",
+    quit = "q",
+  },
+  symbol_in_winbar = {
+    enable = true,
+    in_custom = false,
+  },
 })
