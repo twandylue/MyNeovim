ca072810366543d76bb4f416d0d043fd3523b498
AndyLu <laserlue@gmail.com>
2022-12-19 10:41:08 +0800

    feat: remove the plugin: nvim-obsidian

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -303,9 +303,6 @@ packer.startup({
       requires = "nvim-lua/plenary.nvim",
     })
 
-    -- nvim.obsidian
-    use({ "epwalsh/obsidian.nvim", tag = "v1.*" })
-
     -- incline.nvim
     use("b0o/incline.nvim")
 
