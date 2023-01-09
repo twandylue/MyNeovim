a9665acf215bd12f972e5b5999e6a6a5a22623a3
AndyLu <laserlue@gmail.com>
2023-01-09 10:35:53 +0800

    config: update nvim-bufferline.lua

--- lua/plugin-config/nvim-bufferline.lua
+++ lua/plugin-config/nvim-bufferline.lua
@@ -1,39 +1,50 @@
 local status, bufferline = pcall(require, "bufferline")
 if not status then
-	print("bufferline is not installed")
-	return
+  print("bufferline is not installed")
+  return
 end
 
 bufferline.setup({
-	options = {
-		mode = "tabs",
-		separator_style = "thin",
-		always_show_bufferline = false,
-		show_buffer_close_icons = true,
-		show_close_icon = true,
-		color_icons = true,
-	},
-	highlights = {
-		separator = {
-			fg = "#073642",
-			bg = "#002b36",
-		},
-		separator_selected = {
-			fg = "#073642",
-		},
-		background = {
-			fg = "#657b83",
-			bg = "#002b36",
-		},
-		buffer_selected = {
-			fg = "#fdf6e3",
-			-- bg = '#e62c35'
-			-- undercurl = true
-			underline = true,
-			-- italic = true
-		},
-		fill = {
-			bg = "#073642",
-		},
-	},
+  options = {
+    mode                    = "tabs",
+    numbers                 = "ordinal",
+    -- close_command           = "bdelete! %d",
+    separator_style         = "thick",
+    -- separator_style         = { "", "" },
+    indicator               = {
+      icon = '▎',
+      style = 'icon',
+    },
+    show_tab_indicators     = true,
+    modified_icon           = '●',
+    close_icon              = '',
+    show_buffer_close_icons = true,
+    diagnostics             = "nvim_lsp",
+    show_close_icon         = true,
+    color_icons             = true,
+    always_show_bufferline  = true,
+  },
+  highlights = {
+    separator = {
+      fg = "#073642",
+      bg = "#002b36",
+    },
+    separator_selected = {
+      fg = "#073642",
+    },
+    background = {
+      fg = "#657b83",
+      bg = "#002b36",
+    },
+    buffer_selected = {
+      fg = "#fdf6e3",
+      -- bg = '#e62c35'
+      -- undercurl = true
+      underline = true,
+      -- italic = true
+    },
+    fill = {
+      bg = "#073642",
+    },
+  },
 })
