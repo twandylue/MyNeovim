1f7009d293f3a1696c51309499b8e3e2a942502e
AndyLu <laserlue@gmail.com>
2023-01-09 23:02:19 +0800

    refactor: update plugin source: kyazdani42/nvim-tree.lua to nvim-tree/nvim-tree.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -51,8 +51,10 @@ packer.startup({
 
     -- nvim-tree
     use({
-      "kyazdani42/nvim-tree.lua",
-      requires = "kyazdani42/nvim-web-devicons",
+      "nvim-tree/nvim-tree.lua",
+      requires = {
+        "nvim-tree/nvim-web-devicons", -- optional, for file icons
+      },
     })
 
     -- nvim-netrw
