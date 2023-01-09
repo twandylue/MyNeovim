28fb8c35e50a781e0d41cca7d3fae94c83b89ea4
AndyLu <laserlue@gmail.com>
2023-01-09 19:27:27 +0800

    feat: use nvim-oil as new file manager

--- init.lua
+++ init.lua
@@ -6,7 +6,8 @@ require("colorscheme")
 
 -- plugins settings
 -- require("plugin-config.nvim-tree")
-require("plugin-config.nvim-netrw")
+-- require("plugin-config.nvim-netrw")
+require("plugin-config.nvim-oil")
 require("plugin-config.nvim-gitsigns")
 require("plugin-config.nvim-git-blame")
 require("plugin-config.nvim-neogit")
