8863aa47cf56943a57bd12433601c5558481d75a
AndyLu <laserlue@gmail.com>
2023-07-31 17:50:50 +0800

    style: format code
    Format code.

--- lua/plugin-config/nvim-excahnge.lua
+++ lua/plugin-config/nvim-excahnge.lua
@@ -1,22 +1,22 @@
 local status, nvimsubstitute = pcall(require, "substitute")
 if not status then
-	print("substitute is not installed")
-	return
+  print("substitute is not installed")
+  return
 end
 
 nvimsubstitute.setup({
-	on_substitute = nil,
-	yank_substituted_text = false,
-	range = {
-		prefix = "s",
-		prompt_current_text = false,
-		confirm = false,
-		complete_word = false,
-		motion1 = false,
-		motion2 = false,
-		suffix = "",
-	},
-	exchange = {
-		motion = false,
-	},
+  on_substitute = nil,
+  yank_substituted_text = false,
+  range = {
+    prefix = "s",
+    prompt_current_text = false,
+    confirm = false,
+    complete_word = false,
+    motion1 = false,
+    motion2 = false,
+    suffix = "",
+  },
+  exchange = {
+    motion = false,
+  },
 })
