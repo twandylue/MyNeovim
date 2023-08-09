bfde10e01c5a729ed97d79a0f89174f0ad17df0e
AndyLu <laserlue@gmail.com>
2023-08-09 00:10:23 -0600

    feat(copilot): use tab
    Use tab with Copilot.

--- lua/plugin-config/nvim-copilot.lua
+++ lua/plugin-config/nvim-copilot.lua
@@ -1,2 +1,15 @@
--- vim.g.copilot_filetypes = { xml = false }
-vim.api.nvim_command([[highlight CopilotSuggestion guifg=#555555 ctermfg=8]])
+vim.g.copilot_filetypes = {
+  ["*"] = false,
+  ["javascript"] = true,
+  ["typescript"] = true,
+  ["lua"] = false,
+  ["rust"] = true,
+  ["c"] = true,
+  ["c#"] = true,
+  ["c++"] = true,
+  ["go"] = true,
+  ["python"] = true,
+}
+
+-- vim.api.nvim_command([[highlight CopilotSuggestion guifg=#555555 ctermfg=8]])
+vim.g.copilot_assume_mapped = true
