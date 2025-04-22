093d737bb0da08d540c0c4f9b0ec8207a30c65bc
AndyLu <laserlue@gmail.com>
2025-04-22 08:10:09 -0600

    feat: disable plugins for vscode neovim

--- lua/vscode-plugins.lua
+++ lua/vscode-plugins.lua
@@ -14,26 +14,26 @@ local plugins = {
     end,
   },
 
-  -- nvim-treesitter
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("plugin-config.nvim-treesitter")
-    end,
-  },
-
-  "nvim-treesitter/playground",
-  "nvim-lua/plenary.nvim",
+  -- -- nvim-treesitter
+  -- {
+  --   "nvim-treesitter/nvim-treesitter",
+  --   build = ":TSUpdate",
+  --   config = function()
+  --     require("plugin-config.nvim-treesitter")
+  --   end,
+  -- },
+  --
+  -- "nvim-treesitter/playground",
+  -- "nvim-lua/plenary.nvim",
   -- vim sugar
   -- "tpope/vim-fugitive",
   "tpope/vim-eunuch",
   "tpope/vim-unimpaired",
   "tommcdo/vim-exchange",
-  -- Comment in header file in c++
-  "vim-scripts/DoxygenToolkit.vim",
-  -- Open big files
-  "LunarVim/bigfile.nvim",
+  -- -- Comment in header file in c++
+  -- "vim-scripts/DoxygenToolkit.vim",
+  -- -- Open big files
+  -- "LunarVim/bigfile.nvim",
 }
 
 return plugins
